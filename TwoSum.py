7d5886a72afb605c830655f899a2057f13a20c5c
jaya5591 <96909946+jaya5591@users.noreply.github.com>
2022-10-07 22:01:44 +0530

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,23 @@
+a=[1,2,6,4]
+target=5
+def twosum(a,target):
+  """
+  x+y=target==> y=target-x
+                4=5-1 ,4 not in so add {1:0}
+                3=5-2 ,3 not in {} so add {1:0,2:1}
+               -1=5-6 ,-1 not in {} so add {1:0,2:1,6:2}
+                1=5-4 ,  1 found in {1:0,2:1,6:2}
+                return current index and index of 1
+  """
+  buff={}
+  for i,num in enumerate(a):
+    v=target-num
+    if v not in buff:
+       buff[num]=i
+    else:
+      return [i,buff[v]]
+
+res=twosum(a,target)
+print(res)
+    
+                
